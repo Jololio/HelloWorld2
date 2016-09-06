d607c28c2d71fae3df8fb23a92facfdde23ef70b
Jololio <jala9132@740s-pl-120.local>
2016-09-06 12:17:46 -0600

    Improved Hello World
    Cleaner code, added 2 texts on the same line and comments in the code.

---
+++ src/HellowWorld.java
@@ -0,0 +1,13 @@
+public class HellowWorld 
+	{
+		/**
+		 * Is this how to comment?
+		 * Please don't break my code.
+		 * @param args
+		 */
+		public static void main(String[] args)
+		{
+			System.out.print("Lol time for coding.");
+			System.out.println(" Is this how to write text on the same line?");
+		}
+	}
